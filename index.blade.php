cd5902d4917f9933ac7b6274fdc682551b8d06ef
mayconsantos <maycons523@gmail.com>
2022-07-09 13:44:49 -0300

    Dados

---
+++ index.blade.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html><html lang="pt-br">
+    <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Maycon Santos - TI">
+
+    <title>GRUPO PINTOS LTDA</title>
+    <meta name="description" content="GRUPO PINTOS LTDA - Teresina PI">
+    <meta name="keywords" content="PINTOS LTDA, Magento PINTOS LTDA, Ninho PINTOS LTDA">
+    <link rel="shortcut icon" href="imagens/favicon.png">
+
+    <!-- FONT ROBOTO -->
+    <link rel="preconnect" href="https://fonts.gstatic.com/">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&amp;display=swap" rel="stylesheet">
+
+    <!-- FONT AWESOME (ICONS) -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+    <style>
+
+    .w-100 { height: 25vh; }
+
+    .carousel-indicators{ margin-bottom: 0; }
+
+    .fale { text-shadow: 1px 0 0 black; color: grey; font-family: sans-serif;}
+
+    .card { flex-direction: row; border: none; }
+
+    .card-body { padding: 0px 0px 0px 1rem;}
+
+    .card-text { font-size: small; }
+    
+    .card-img-left { max-width: 170px; min-width: 140px;}
+
+    .g-5, .gx-5 { --bs-gutter-x: 0; }
+
+    #cabecalho { text-align: center; }
+
+    .letrero { max-width: 13%; }
+
+    .card { max-height: 20vh; }
+
+    img.logo { max-width: 25%; }
+
+    .d-flex img {
+        max-width: 75%;
+    }
+
+    .avatarButton { outline: none; display: flex; justify-content: center; align-items: center;
+        bottom: 0; 
+        right: 0; 
+        height: 10vh;
+         width: 10vh; 
+        -webkit-box-shadow: 0px 0px 10px 1px rgba(0,0,0,0.5); 
+        -moz-box-shadow: 0px 0px 10px 1px rgba(0,0,0,0.5); 
+        box-shadow: 0px 0px 10px 1px rgba(0,0,0,0.5); 
+        border-radius: 100%; 
+        margin: 1rem;
+        z-index: 10000; 
+        cursor: pointer; 
+        overflow: hidden; 
+        border: 2px solid #006BE2; 
+        background-color: #006BE2; 
+        padding: 0; 
+        float: right;
+        margin: 0; 
+    }
+
+    h6.card-title {margin: 0;}
+
+    .bot {display: grid; position: fixed; bottom: 1.5rem; justify-items: center;}
+
+    .botAvatar {display: block; width: 100%; height: 100%; object-fit: cover;}    
+
+    .contato {margin-left: 10rem; font-size: 1rem; width: max-content;}
+
+    .social { padding: 0px; margin: 0px; display: flex; justify-content: center; list-style: none; }
+
+    @media screen and (max-width: 1400px) {
+    .letrero { max-width: 12%; }
+    .contato {margin-left: 10rem; font-size: .8rem;}
+    }
+
+    @media screen and (max-width: 1200px) {
+    .contato {margin-left: 10rem; font-size: .6rem;}
+    }
+
+    @media screen and (max-width: 992px) {
+    .letrero { max-width: 11.5%; }
+    .card-text { font-size: x-small; }
+    .contato {margin-left: 5rem; font-size: .5rem;}
+    .avatarButton {height: 8vh ;width: 8vh;}
+    .w-100 { height: 20vh;}
+    }
+
+    @media screen and (max-width: 760px) {
+    .contato {margin-left: 4rem; font-size: .4rem;}
+    .title { font-size: x-small;}
+    .w-100 { height: 18vh;}
+    }
+
+    @media screen and (max-width: 540px) {
+    .contato {margin-left: 4rem; font-size: .3rem;}
+    .w-100 { height: 15vh;}
+    }
+
+    @media screen and (max-width: 414px) {
+    .avatarButton {height: 5vh ;width: 5vh;}
+    .contato {margin-left: 4rem; font-size: .2rem;}
+    .w-100 { height: 12vh;}
+    }
+
+    body { min-width: 375px;}
+
+}
+    </style>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script>
+    window.waButtonConfig = {
+      retailerId: 'qlKYP33pYe',
+        wabNumber: '558621074000',
+        index: '1',
+        redirectOnly: false,
+        message: 'Olá! Gostaria de ser atendido.',
+        codeMessage: 'O código do meu atendimento é:',
+        button: {
+            textColor: '#f3f3f3',
+            borderColor: '#f3f3f3',
+            middleColor: '#23aa2a',
+            phoneColor: '#f3f3f3',
+            position: 'right',
+            type: 'button',
+            text: 'Compre pelo WhatsApp'
+        }
+    }
+  </script>
+  <script defer="defer" src="js/magento.bundle.js"></script>
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12" id="cabecalho">
+                <img class="img-fluid logo" alt="GRUPO PINTOS LTDA" src="img/logopintosltda.png">
+
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                            <img class="d-block w-100" alt="Banner" src="img/index-prov-pintos_04.png">
+                      </div>
+                      <div class="carousel-item">
+                        <a href="https://boleto.pintos.com.br/" target="_blank">
+                            <img class="d-block w-100" alt="Boleto" src="img/index-prov-pintos_25.png">
+                        </a>
+                      </div>
+                      <div class="carousel-item">
+                        <a href="https://pintos.upcatalogo.com.br/" target="_blank">
+                            <img class="d-block w-100" alt="Boleto" src="img/index-prov-pintos_64.jpeg">
+                        </a>
+                      </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+
+                <img class="img-fluid" alt="Departamentos" src="img/index-prov-pintos-dep.png">
+            
+            </div>
+            
+            <div class="col text-center my-5">
+                <h1>Conheça nossas Lojas!</h1>
+            </div>
+                
+        </div>
+
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-5">
+            <div class="col">
+              <div class="card">
+                <img src="img/index-prov-pintos_29.png" class="card-img-left" alt="Pintos Magazine">
+                <div class="card-body">
+                    <h6 class="card-title">Pintos Magazine</h6>
+                    <p class="card-text">
+                        Rua Álvaro Mendes, 1237 - Centro<br>
+                        CEP: 64.000-060, Teresina-PI<br>
+                        Fone/WhatsApp: 86 2107-4000<br>
+                        Horário de Funcionamento:<br>
+                        Segunda à sexta-feira 8h às 17:30h<br>
+                        Sábado: 8h30 às 13h30
+                    </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="img/index-prov-pintos_31.png" class="card-img-left" alt="Pintos Rio Branco">
+                <div class="card-body">
+                    <h6 class="card-title">Pintos Rio Branco</h6>
+                    <p class="card-text">
+                        Praça Rio Branco, 244 - Centro<br>
+                        CEP: 64.000-140, Teresina-PI<br>
+                        Fone: 86 3194-6700<br>
+                        Horário de Funcionamento:<br>
+                        Segunda à sexta-feira 8h às 17h:30<br>
+                        Sábado: 8h30 às 13h30
+                    </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="img/index-prov-pintos_35.png" class="card-img-left" alt="Pintos Riverside">
+                <div class="card-body">
+                    <h6 class="card-title">Pintos Riverside</h6>
+                    <p class="card-text">
+                        Av. Ininga, 1201 - Jóquei<br>
+                        CEP: 64.048-110, Teresina-PI<br>
+                        Fone: (86) 2107-4030<br>
+                        Horário de Funcionamento:<br>
+                        Segunda à sabado 9h00 às 21h00<br><br>
+                    </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="img/index-prov-pintos_37.png" class="card-img-left" alt="Pintos Calçados">
+                <div class="card-body">
+                    <h6 class="card-title">Pintos Calçados</h6>
+                    <p class="card-text">
+                        Rua Barroso, 17 - Centro<br>
+                        CEP: 64.002-245, Teresina-PI<br>
+                        Fone: (86) 2107-4020<br>
+                        Horário de Funcionamento:<br>
+                        Segunda à sexta-feira 8h às 17:30h<br>
+                        Sábado: 8h30 às 13h30
+                    </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img/index-prov-pintos_39.png" class="card-img-left" alt="Pintos Frei Serafim">
+                    <div class="card-body">
+                        <h6 class="card-title">Pintos Frei Serafim</h6>
+                        <p class="card-text">
+                            Av. Frei Serafim, 2170 - Centro<br>
+                            CEP: 64.000-020, Teresina-PI<br>
+                            Fone: (86) 2107-4040<br>
+                            Horário de Funcionamento:<br>
+                            Segunda à sexta-feira 8h às 17h<br>
+                            Sábado: 8h30 às 13h30
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img/index-prov-pintos_42.png" class="card-img-left" alt="Pintos Timon">
+                    <div class="card-body">
+                        <h6 class="card-title">Pintos Timon</h6>
+                        <p class="card-text">
+                            Av Presidente Médici, 500 - Parque Piauí<br>
+                            CEP: 65.631-390, Timon-MA<br>
+                            Fone/WhatsApp: (86) 2107-4006<br>
+                            Horário de Funcionamento:<br>
+                            Segunda à sexta-feira 8h às 17:30h<br>
+                            Sábado: 8h30 às 13h30
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img/index-prov-pintos_43.png" class="card-img-left" alt="Pintos Shopping">
+                    <div class="card-body">
+                        <h6 class="card-title">Pintos Shopping</h6>
+                        <p class="card-text">
+                            Rua das Tulipas, 500 - Jóquei<br>
+                            CEP: 64.049-140, Teresina-PI<br>
+                            Fone: (86) 3194-3500<br>
+                            Horário de Funcionamento:<br>
+                            Segunda à Sábado: 9h00 às 21h00<br><br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img/index-prov-pintos_45.png" class="card-img-left" alt="Pintos Dirceu">
+                    <div class="card-body">
+                        <h6 class="card-title">Pintos Dirceu</h6>
+                        <p class="card-text">
+                            Av Joaquim Nelson, 3159 - Parque Ideal<br>
+                            CEP: 64.078-625, Teresina-PI<br>
+                            Fone/WhatsApp: (86) 3194-3549<br>
+                            Horário de Funcionamento:<br>
+                            Segunda à sexta-feira 8h às 17:30h<br>
+                            Sábado: 8h30 às 13h30
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12" id="rodape">
+            <img class="img-fluid my-5" alt="Banner" src="img/index-prov-pintos_48.png">
+        </div>
+
+        <div class="row">
+            <div class="col-4">
+                <img class="img-fluid" alt="GRUPO PINTOS LTDA" src="img/logopintosltda2.png">
+                <div class="d-flex">
+                    <div class="col">
+                        <a href="http://instagram.com/lojaspintos" target="_blank">
+                            <img class="social" alt="GRUPO PINTOS LTDA" src="img/index-prov-pintos_61.png">
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="https://www.youtube.com/channel/UCez7f_QLeZW_0CpH1LiWJyg/videos" target="_blank">
+                            <img class="social" alt="GRUPO PINTOS LTDA" src="img/index-prov-pintos_60.png">
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="https://www.facebook.com/lojaspintos" target="_blank">
+                            <img class="social" alt="GRUPO PINTOS LTDA" src="img/index-prov-pintos_62.png">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col"> 
+                <div class="row">
+                    <img class="img-fluid" alt="Banner" src="img/index-prov-pintos_50.png">
+                    <div class="col position-absolute contato text-white">
+                        <h5 class="title">Contatos:</h5>
+                        Rua Álvaro Mendes, 1237 - Centro CEP:64.000-060, Teresina-PI<br>
+                        Fone/WhatsApp: 86 2107-4000<br>
+                        Horário de Funcionamento: Segunda à sexta-feira > 8h às 17h<br>
+                        atendimento@pintos.com.br
+                    </div>   
+                    <div class="text-center bot">    
+                        <button class="avatarButton" id="avatarButton">
+                            <img class="wc-greeting-image botAvatar" src="https://people-prod-files.s3.us-west-2.amazonaws.com/20200702195606-62d94bcf-7677-4d96-8e05-a94246c84bc4.jpeg" >
+                        </button> 
+                        <p class="fale"><b>Fale</b> com <br> a <b>PINTOS</b></p>
+                    </div>
+                </div>
+                
+            </div> 
+        </div>
+
+        <footer class="footer pt-2">
+            <div class="container">
+              <div class="row">
+                <div class="col-8 mx-auto  mt-1">
+                  <p class="mb-0 text-secondary text-center"><small>
+                    <a href="https://github.com/mayconsantos" target="_blank">
+                        <span class="text-lg fab fa-github"></span>
+                    </a>
+                    {{ __("Corpyright") }} © <script>
+                      document.write(new Date().getFullYear())
+                    </script> {{ __("Created by")}} TI PINTOS.
+                  <small></p>
+                </div>
+              </div>
+            </div>
+          </footer>
+
+    </div>
+
+    
+</body>
+</html>
+
+
+
